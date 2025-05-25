cccb7af291f5ba981d7777d056ef6b775f35f686
javierrc8 <jarocorodriguez@gmail.com>
2025-05-25 11:36:44 +0200

    fix: minor changes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Running Alembic migrations..."
+cd /app/backend
+
+echo "🛠 Running Alembic migrations..."
 alembic upgrade head
+echo "Alembic migrations completed"
 
 echo "Starting app..."
 exec uvicorn main:app --host 0.0.0.0 --port 8000
